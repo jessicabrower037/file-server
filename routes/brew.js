2e2400d6f2afd81050ba868941538eb5d7e75e01
jessicabrower037 <jessicabrower037@gmail.com>
2022-12-21 17:17:49 -0500

    added first route

---
+++ routes/brew.js
@@ -0,0 +1,16 @@
+// The server attempts to brew coffee, but fails.
+
+function brew(app) {
+    app.get(
+        "/brew",
+        (request, response) => {
+            let responseMessage = "<h1>I'm a teapot, so I cannot brew coffee!</h1>";
+            response
+                .status(418)
+                .send(responseMessage);
+        }
+    );
+
+}
+
+module.exports = brew;
